9c4ed84cb1f4fa015d8c813419e202fcc82bf17d
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-24 14:00:23 +0300

    Add project files.

---
+++ CSharp10/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] dizi = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+
+Console.WriteLine("Dizi eleman sayısı = " + dizi.Length);
+
+for (int i = 0; i < dizi.Length; i++)
+{
+    Console.WriteLine($"Dizinin {i + 1} . elemanı = {dizi[i]} ");
+}
+
+Console.WriteLine("Dizinin en küçük elemanı = " + dizi.Min());
+Console.WriteLine("Dizinin en büyük elemanı = " + dizi.Max());
